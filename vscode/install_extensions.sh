7796ada6a226fb37c54530192cc9e7968ceb82fb
Hiko <h.d.kibar@outlook.de>
2020-10-31 21:58:28 +0100

    added vscode settings and extensions

---
+++ vscode/install_extensions.sh
@@ -0,0 +1,10 @@
+code --install-extension yzhang.dictionary-completion
+code --install-extension teabyii.ayu
+code --install-extensionms-azuretools.vscode-docker
+code --install-extension james-yu.latex-workshop
+code --install-extension yzane.markdown-pdf
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension ban.spellright
+code --install-extension vscodevim.vim
+code --install-extension valentjn.vscode-ltex
