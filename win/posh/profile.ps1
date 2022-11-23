238c705742405ed5fbbc4b057c722c72a60f25be
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2022-11-24 00:54:53 +0100

    update

--- win/posh/profile.ps1
+++ win/posh/profile.ps1
@@ -3,7 +3,7 @@ function runInContainer {podman run --rm -h pluto -v ./:/home/x -it pluto}
 Set-Alias -Name dev -Value runInContainer
 
 # --- SCRIPTS
-Add-PathVariable "${env:HOMEPATH}\.local\bin}"
+$env:Path += ";${env:HOMEPATH}\.local\bin}"
 
 # --- UNIX LIKE
 # From https://github.com/mikemaccana/powershell-profile
@@ -11,17 +11,6 @@ Add-PathVariable "${env:HOMEPATH}\.local\bin}"
 # From https://github.com/Pscx/Pscx
 function sudo(){
 	Invoke-Elevated @args
-}
-
-# Like Unix touch, creates new files and updates time on old ones
-# PSCX has a touch, but it doesn't make empty files
-Remove-Alias touch
-function touch($file) {
-	if ( Test-Path $file ) {
-		Set-FileTime $file
-	} else {
-		New-Item $file -type file
-	}
 }
 
 # Truncate homedir to ~
