ff5a6b3041e9f391c39b9dce4c1a65efba111afd
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2022-12-23 01:16:06 +0100

    Prompt like on linux. Vi mode. Alias to clear

--- win/posh/profile.ps1
+++ win/posh/profile.ps1
@@ -1,20 +1,30 @@
-# --- SCRIPTS
-$env:Path += ";${env:userprofile}\.local\bin"
-
-# --- UNIX LIKE
-# From https://github.com/mikemaccana/powershell-profile
-
-# Truncate homedir to ~
-function limit-HomeDirectory($Path) {
-  $Path.Replace("$home", "~")
-}
-
-# Must be called 'prompt' to be used by pwsh
-# https://github.com/gummesson/kapow/blob/master/themes/bashlet.ps1
+# --- TERMINAL
+Set-PSReadlineOption -EditMode vi
 function prompt {
+  # username.computername currdirectory.gitbranch $
+  # hiko.nep dot.main $
   $realLASTEXITCODE = $LASTEXITCODE
-  Write-Host $(limit-HomeDirectory("$pwd")) -ForegroundColor Yellow -NoNewline
-  Write-Host " $" -NoNewline
+
+  Write-Host "$env:username".ToLower() -ForegroundColor Gray -NoNewline
+  Write-Host ".$env:computername ".ToLower() -ForegroundColor DarkYellow -NoNewline
+  Write-Host $(Get-Item -Path $(Get-Location)).BaseName.ToLower() -ForegroundColor Gray -NoNewline
+  if ($GITBRANCH = git branch --show-current 2> $null) {
+    Write-Host ".$GITBRANCH"  -ForegroundColor DarkYellow -NoNewline
+    # Show number of issues
+    # TOO SLOW !
+    # if ($ISSUES = gh issue list | Measure-Object -Line | Select-Object -ExpandProperty Lines 2> $null) {
+    #   Write-Host " ($ISSUES)"  -ForegroundColor Red -NoNewline
+    # }
+  }
+  Write-Host ""
+  Write-Host "$" -ForegroundColor DarkRed -NoNewline
+
   $global:LASTEXITCODE = $realLASTEXITCODE
   Return " "
 }
+
+# --- SCRIPTS
+$env:Path += ";${env:userprofile}\.local\bin"
+
+# --- ALIAS
+Set-Alias -Name k -Value clear
