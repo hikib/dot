e72689a353bedaf7678757537d4ff6f8b2c19c4f
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2023-01-06 22:56:46 +0100

    removed nested path

--- win.posh/setup.ps1
+++ win.posh/setup.ps1
@@ -1,3 +1,3 @@
 # Powershell Profile
 $profileLocation = Split-Path $PROFILE.CurrentUserCurrentHost
-New-Item -ItemType SymbolicLink -Path "$profileLocation\profile.ps1" -Target "$PSScriptRoot\posh\profile.ps1" -Force
+New-Item -ItemType SymbolicLink -Path "$profileLocation\profile.ps1" -Target "$PSScriptRoot\profile.ps1" -Force
--- win.wsl/setup.ps1
+++ win.wsl/setup.ps1
@@ -1,3 +1,3 @@
 # WSL settings
 $SETTINGSPATH = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-New-Item -ItemType SymbolicLink -Path "$SETTINGSPATH\settings.json" -Target "$PSScriptRoot\wsl\settings.json" -Force
+New-Item -ItemType SymbolicLink -Path "$SETTINGSPATH\settings.json" -Target "$PSScriptRoot\settings.json" -Force
