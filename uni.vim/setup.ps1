e747b6b8a21ae5e6b03cf6b6ceee1646f24398d0
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2023-02-05 19:57:08 +0100

    not working. issue with overwriting, if links exists.

--- uni.vim/setup.ps1
+++ uni.vim/setup.ps1
@@ -1,15 +1,46 @@
+# Set up vim on WIN
+# Requires git-bash, therefore under uni.vim
 
-New-Item -ItemType SymbolicLink -Path "$home\.vimrc" -Target "$PSScriptRoot\vimrc" -Force
-New-Item -ItemType SymbolicLink -Path "$home\.vim\ftplugin" -Target "$PSScriptRoot\vim\ftplugin" -Force
+$vrc = "$home\.vimrc"
+if (Test-Path -LiteralPath $vrc -PathType Leaf) { Remove-Item $vrc }
+# cmd /c mklink $vrc "$PSScriptRoot\vimrc"
+New-Item -ItemType SymbolicLink `
+  -Path $vrc `
+  -Target "$PSScriptRoot\vimrc" `
+  -Force
+
+$vimdir =  "$home\.vim"
+$vimdir_exists = Test-Path -LiteralPath $vimdir -PathType Container
+if ( -not $vimrdir_exists ) {
+  New-Item `
+    -ItemType Directory `
+    -Path $vimdir
+}
+
+$DOTFILES = "after", `
+            "colors", `
+            "ftdetect.vim", `
+            "ftplugin", `
+            "functions.vim", `
+            "mappings.vim", `
+            "spell"
+foreach ($DOTFILE in $DOTFILES)
+{
+  $df = "$home\.vim\$DOTFILE"
+  if (Test-Path -LiteralPath $df -PathType Leaf) {
+    Remove-Item -Path $df -force
+    cmd /c mklink $df "$PSScriptRoot\vim\$DOTFILE"
+  } else {
+    Remove-Item -Path $df -recurse -force
+    cmd /c mklink /d $df "$PSScriptRoot\vim\$DOTFILE"
+  }
+}
 
 $vimplug = "$home\.vim\autoload\plug.vim"
 $plug_exists = Test-Path -LiteralPath $vimplug -PathType Leaf
-
-if (-not $plug_exists) {
+if ( -not $plug_exists ) {
   Write-Host "Vim-Plug does not exist! Curling vim-plug... "
   $vimpluggit = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   curl -fLo "$vimplug" --create-dirs $vimpluggit
 }
-
-Read-Host -Prompt "Press Enter to exit"
-
+Read-host "Press Enter"
