35d98bc1bb9668dc0cc4af9137d2c5d2627b685e
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2023-10-29 15:14:55 +0100

    [fix] reformat. added browser env.

--- win.posh/profile.ps1
+++ win.posh/profile.ps1
@@ -1,41 +1,48 @@
 # ---- ALIASES
 Set-Alias -Name k -Value clear
 Set-Alias -Name vi -Value nvim
-Set-Alias -Name ff -Value firefox
 
 # ---- SCRIPTS
-$env:Path += ";${env:userprofile}\.local\bin"
-$env:Path += ";C:\msys64\mingw64\bin"
-$env:Path += ";C:\lua-language-server\bin"
-$env:MOME = "C:\Users\Hiko\repos\github.com\hikib\mome\docs"
+Set-Alias -Name ff -Value firefox
+Set-Alias -Name yt -Value youtube
+
+# ---- ENVIRONMENT VARIABLES
+$env:BROWSER = "$env:ProgramFiles\Mozilla Firefox\firefox.exe"
 $env:EDITOR = "nvim"
+$env:MOME = "$env:userprofile\repos\github.com\hikib\mome\docs"
+
+# ---- PATH ADDITIONS
+$env:Path += ";${env:userprofile}\.local\bin" # my scripts
+$env:Path += ";C:\lua-language-server\bin" # nvim
+$env:Path += ";C:\msys64\mingw64\bin" # git-bash
 
 # ---- TERMINAL
 Set-PSReadlineOption -EditMode vi
-Set-Location "${env:userprofile}/repos/github.com/hikib/"
+Set-Location "$env:userprofile/repos/github.com/hikib/"
 
-function prompt {
-  # format:
-  # username.computername currDirectory.gitBranch $
-  # example:
-  # hiko.nep dot.main $
+function prompt
+{
+    # format:
+    # username.computername currDirectory.gitBranch $
+    #
+    # example:
+    # hiko.nep dot.main $
 
-  $realLASTEXITCODE = $LASTEXITCODE
+    $realLASTEXITCODE = $LASTEXITCODE
+    $currDirectory = $($executionContext.SessionState.Path.CurrentLocation).ToString().Replace($env:userprofile, '~')
+    $hasGitBranch = $gitBranch = git branch --show-current 2> $null
 
-  # $currDirectory = $(Get-Item -Path $(Get-Location)).BaseName.ToLower() # current directory name only
-  $currDirectory = $($executionContext.SessionState.Path.CurrentLocation).ToString().Replace($env:userprofile, '~')
-  $hasGitBranch = $gitBranch = git branch --show-current 2> $null # assigns "git branch..." to $gitBranch
+    Write-Host "$env:username".ToLower() -ForegroundColor Gray -NoNewline
+    Write-Host ".$env:computername".ToLower() -ForegroundColor DarkYellow -NoNewline
+    Write-Host " $currDirectory" -ForegroundColor Gray -NoNewline
+    if ($hasGitBranch)
+    {
+        Write-Host ".$gitBranch"  -ForegroundColor DarkYellow -NoNewline
+    }
+    Write-Host ""
+    Write-Host "$" -ForegroundColor DarkRed -NoNewline
 
-  Write-Host "$env:username".ToLower() -ForegroundColor Gray -NoNewline
-  Write-Host ".$env:computername".ToLower() -ForegroundColor DarkYellow -NoNewline
-  Write-Host " $currDirectory" -ForegroundColor Gray -NoNewline
-  if ($hasGitBranch) {
-    Write-Host ".$gitBranch"  -ForegroundColor DarkYellow -NoNewline
-  }
-  Write-Host ""
-  Write-Host "$" -ForegroundColor DarkRed -NoNewline
-
-  $global:LASTEXITCODE = $realLASTEXITCODE
-  Return " "
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Return " "
 }
 
