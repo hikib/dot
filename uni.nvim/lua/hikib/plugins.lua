e8874cc5461c53d3ed51f9dcc764d8a94bfb8669
hikib <43708222+hikib@users.noreply.github.com>
2023-12-25 01:21:55 +0100

    [build] pkgmgr changed from packer to lazyvim. removed unused plugins

--- uni.nvim/lua/hikib/plugins.lua
+++ uni.nvim/lua/hikib/plugins.lua
@@ -1,59 +1,56 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- PLUGIN MANAGER
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-    -- PACKER
-    use 'wbthomason/packer.nvim'
+require("lazy").setup({
+     { 'nvim-treesitter/nvim-treesitter', cmd = 'TSUpdate' },
+     
+     { 'rose-pine/neovim',
+         name = 'rose-pine',
+         config = function()
+             vim.cmd('colorscheme rose-pine')
+         end
+     },
+ 
+     -- EXTRA
+     -- 'github/copilot.vim',
+     -- 'mbbill/undotree',
+     -- 'tpope/vim-fugitive',
+     'tpope/vim-commentary',
+     'karb94/neoscroll.nvim',
+ 
+     -- LSP
+     { 'VonHeikemen/lsp-zero.nvim',
+         branch = 'v3.x',
+         dependencies = {
+             -- LSP SUPPORT
+             'neovim/nvim-lspconfig',
+             'williamboman/mason.nvim',
+             'williamboman/mason-lspconfig.nvim',
+ 
+             -- AUTOCOMPLETION
+             'hrsh7th/cmp-buffer',
+             'hrsh7th/cmp-nvim-lsp',
+             'hrsh7th/cmp-nvim-lua',
+             'hrsh7th/cmp-path',
+             'hrsh7th/nvim-cmp',
+             'saadparwaiz1/cmp_luasnip',
+ 
+             --  SNIPPETS
+             'L3MON4D3/LuaSnip',
+             'rafamadriz/friendly-snippets',
+         } },
+})
 
-    -- BASICS
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-
-
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
-
-    -- EXTRA
-    use('github/copilot.vim')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('tpope/vim-commentary')
-    use('karb94/neoscroll.nvim')
-
-    -- LSP
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-
-            -- LSP SUPPORT
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- AUTOCOMPLETION
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-            { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/nvim-cmp' },
-            { 'saadparwaiz1/cmp_luasnip' },
-
-            --  SNIPPETS
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-        }
-
-    }
-end)
---
+++ uni.nvim/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+THISDIR=$(dirname $(readlink -f "$0"))
+DOTFILES=(
+  after
+  init.lua
+  lua
+)
+
+mkdir -p "${HOME}/.config/nvim"
+
+for DOTFILE in "${DOTFILES[@]}"; do
+  ln -svf "${THISDIR}/${DOTFILE}" "${HOME}/.config/nvim/${DOTFILE}"
+done
