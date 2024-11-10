0ec2634180c86ca5eccacc536971f47728e30800
hikib <43708222+hikib@users.noreply.github.com>
2024-11-10 23:03:41 +0100

    [add] added keymapping changes
    [feat] added keymap settings

--- uni.w3m/setup.sh
+++ uni.w3m/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-THISDIR=$(dirname $(readlink -f $0))
-mkdir -p $HOME/.w3m
-ln -svf $THISDIR/bookmark.html $HOME/.w3m/bookmark.html
-ln -svf $THISDIR/config $HOME/.w3m/config
-
+THISDIR="$(dirname "$(readlink -f "$0")")"
+mkdir -p "$HOME/.w3m"
+ln -svf "$THISDIR/bookmark.html" "$HOME/.w3m/bookmark.html"
+ln -svf "$THISDIR/config" "$HOME/.w3m/config"
+ln -svf "$THISDIR/keymap" "$HOME/.w3m/keymap"
