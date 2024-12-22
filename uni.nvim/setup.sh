7662d1bebfb518ffb3bd49161d650d96014f2eb5
hikib <43708222+hikib@users.noreply.github.com>
2024-12-22 22:17:57 +0100

    feat: nvim setup for lsp added

--- uni.nvim/setup.sh
+++ uni.nvim/setup.sh
@@ -13,6 +13,16 @@ for DOTFILE in "${DOTFILES[@]}"; do
     ln -svf "${THISDIR}/${DOTFILE}" "${HOME}/.config/nvim/${DOTFILE}"
 done
 
-# nvim -c "TSUpdate" "MasonUpdate" +qa
-# nvim --headless "+Lazy! sync" +qa
-# nvim SOMETMPFILE --headless -c "PylspInstall python-lsp-black" +qa
+nvim --headless -c "TSUpdate" "MasonUpdate" +qa
+nvim --headless "+Lazy! sync" +qa
+nvim SOMETMPFILE --headless -c "MasonInstall \
+	bash-language-server \
+	lua-language-server \
+	marksman \
+	powershell-editor-services \
+	python-lsp-server \
+	shfmt \
+	gopls \
+	yaml-language-server \
+	" \
+    "PylspInstall python-lsp-black" +qa
